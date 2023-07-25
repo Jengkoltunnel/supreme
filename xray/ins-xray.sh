1b8685bf5845bd28c2f201c41f45758675b428e1
Jengkoltunnel <139392019+Jengkoltunnel@users.noreply.github.com>
2023-07-25 12:17:06 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -573,7 +573,8 @@ wget -O restorebot "https://raw.githubusercontent.com/Jengkolonline/bot/main/res
 wget -O backupbot "https://raw.githubusercontent.com/Jengkolonline/bot/main/backupbot.sh" && chmod +x backupbot
 wget -O menu-bot "https://raw.githubusercontent.com/Jengkolonline/bot/main/menu-bot.sh" && chmod +x menu-bot
 wget -O bot-cek-ssws "https://raw.githubusercontent.com/Jengkolonline/bot/main/bot-cek-ssws.sh" && chmod +x bot-cek-ssws
-wget -O backupbot "https://raw.githubusercontent.com/Jengkolonline/bot/main/backupbot.sh" chmod && backupbot
+wget -O backupbot "https://raw.githubusercontent.com/Jengkolonline/bot/main/backupbot.sh" && chmod +x backupbot
+wget -O restorebot "https://raw.githubusercontent.com/Jengkoltunnel/supreme/main/restorebot.sh" && chmod +x restorebot
 sleep 0.5
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "xray/Vmess"
