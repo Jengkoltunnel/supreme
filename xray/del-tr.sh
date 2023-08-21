e0860c920f61f067a5b7764e347436987b04fe9a
Jengkoltunnel <139392019+Jengkoltunnel@users.noreply.github.com>
2023-08-22 00:34:37 +0700

    Update del-tr.sh

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -12,7 +12,7 @@ red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/usr/local/etc/xray/trojanws.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
   if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
         echo -e "\E[44;1;39m     ⇱ Delete Trojan Account ⇲      \E[0m"
@@ -31,7 +31,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/usr/local/etc/xray/trojanws.json")
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
     echo "  User       Expired  " 
   echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-  grep -E "^#! " "/usr/local/etc/xray/trojanws.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
+  grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
     echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
@@ -39,7 +39,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/usr/local/etc/xray/trojanws.json")
     if [ -z $user ]; then
     menu
     else
-    exp=$(grep -wE "^#! $user" "/usr/local/etc/xray/trojanws.json" | cut -d ' ' -f 3 | sort | uniq)
+    exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
     sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
