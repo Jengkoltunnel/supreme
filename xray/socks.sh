f4cb2070d00c76d36d959829d5bfa3ea97c6ac82
Jengkoltunnel <139392019+Jengkoltunnel@users.noreply.github.com>
2023-08-22 03:18:18 +0700

    Add files via upload

---
+++ xray/socks.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
+echo -e " \033[31m╭══════════════════════════════════════════════════════════╮\e[0m"
+echo -e " \033[34m│$NC\033[33m                       MENU Socks5                         $NC\033[34m│\e[0m"
+echo -e " \033[33m╰══════════════════════════════════════════════════════════╯\e[0m"
+echo -e " \033[32m╭══════════════════════════════════════════════════════════╮\e[0m"
+echo -e " \033[35m│$NC ${MB}[01]${NC} ${YB}Create Account Socks5${NC} "
+echo -e " \033[35m│$NC ${MB}[02]${NC} ${YB}Trial Account Socks5${NC} "
+echo -e " \033[35m│$NC ${MB}[03]${NC} ${YB}Extend Account Socks5${NC} "
+echo -e " \033[35m│$NC ${MB}[04]${NC} ${YB}Delete Account Socks5${NC} "
+echo -e " \033[35m│$NC ${MB}[05]${NC} ${YB}Check User Login${NC} "
+echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
+echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
+echo -e ""
+echo -e " ${MB}[0]${NC} ${YB}Back To Menu${NC}"
+echo -e ""
+read -p " Select menu :  "  opt
+echo -e ""
+case $opt in
+1) clear ; add-socks ; exit ;;
+2) clear ; trialsocks ; exit ;;
+3) clear ; extend-socks ; exit ;;
+4) clear ; del-socks ; exit ;;
+5) clear ; cek-socks ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "salah tekan " ; sleep 1 ; socks ;;
+esac
